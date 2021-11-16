e997a374e672a035cd9c118593a84801f0da22f2
Rida El fagrouch <rel-fagr@e2r9p3.1337.ma>
2021-11-16 17:20:14 +0100

    final project

--- ft_strchr.c
+++ ft_strchr.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <string.h>
-//#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
